5478b4da78fa443fed3bf16460bc9d8ca7225b9f
Raul Carreras <raul.e.carreras@gmail.com>
2020-05-13 18:50:45 -0400

    new react app

---
+++ public/precache-manifest.9ebd43465317f18cb2192fcb6e206855.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab14f93716a95ef1b1c175e26659887a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "207152f99c4a5a1f5e41",
+    "url": "/static/css/0.bbaacb0d.chunk.css"
+  },
+  {
+    "revision": "f0f2e7efccb7069f8ddd",
+    "url": "/static/css/4.9c1c01d6.chunk.css"
+  },
+  {
+    "revision": "14da11fd0540ed371352",
+    "url": "/static/css/5.ffc823ad.chunk.css"
+  },
+  {
+    "revision": "542d1ccb016a32a07b2e",
+    "url": "/static/css/6.93f3b060.chunk.css"
+  },
+  {
+    "revision": "24ae1a8e17a9c247ffb9",
+    "url": "/static/css/7.6cafb8e4.chunk.css"
+  },
+  {
+    "revision": "feab5b8c55546fed8401",
+    "url": "/static/css/8.cda79222.chunk.css"
+  },
+  {
+    "revision": "ea88d143cd94680f8672",
+    "url": "/static/css/main.08b7a5b4.chunk.css"
+  },
+  {
+    "revision": "207152f99c4a5a1f5e41",
+    "url": "/static/js/0.ac383b22.chunk.js"
+  },
+  {
+    "revision": "e0f68c133ce330949662",
+    "url": "/static/js/3.b388317a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.b388317a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f2e7efccb7069f8ddd",
+    "url": "/static/js/4.7c1f17da.chunk.js"
+  },
+  {
+    "revision": "14da11fd0540ed371352",
+    "url": "/static/js/5.01e3d3f7.chunk.js"
+  },
+  {
+    "revision": "542d1ccb016a32a07b2e",
+    "url": "/static/js/6.32474706.chunk.js"
+  },
+  {
+    "revision": "24ae1a8e17a9c247ffb9",
+    "url": "/static/js/7.46cd3778.chunk.js"
+  },
+  {
+    "revision": "feab5b8c55546fed8401",
+    "url": "/static/js/8.e2084080.chunk.js"
+  },
+  {
+    "revision": "ea88d143cd94680f8672",
+    "url": "/static/js/main.fdafddfe.chunk.js"
+  },
+  {
+    "revision": "22938aad97a23868e16a",
+    "url": "/static/js/runtime-main.52cfda6d.js"
+  }
+]);
